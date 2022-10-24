4b1c2c3a7eeaf8eb198484627c24c858073033d6
Gabriel Bursi <gabrielcavalarosuporte@gmail.com>
2022-10-24 16:30:23 -0300

    navbar e footer

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import Contato from "./pages/Contato";
 import Empresa from "./pages/Empresa";
 import NovoProjeto from "./pages/NovoProjeto";
+import Projetos from "./pages/Projetos";
 
 function AppRoutes() {
     return (
@@ -13,6 +14,7 @@ function AppRoutes() {
                 <Route path="/contato" element={<Contato />} />
                 <Route path="/empresa" element={<Empresa />} />
                 <Route path="/novo-projeto" element={<NovoProjeto/>} />
+                <Route path="/projetos" element={<Projetos/>} />
             </Routes>
         </>
     );
